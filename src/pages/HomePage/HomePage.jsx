6691dc5bd1d9b8f26056f76b6426a6c5a2f895e4
CarlTobias <113130276+CarlTobias@users.noreply.github.com>
2025-05-04 02:36:01 +0400

    start of homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+
+import { Box, Container, Flex } from "@chakra-ui/react";
 
 const HomePage = () => {
   return (
     <>
-    Hello
-    </>
-  )
-}
+      <Container maxW={"100vw"} position={"relative"}>
+        <Flex justify={"center"}>
+          <Box py={10} mr={6} border={"1px solid black"}>
+            User Feed FYP
+          </Box>
 
-export default HomePage
+          <Box
+            position={"absolute"}
+            top={"0"}
+            right={"10"}
+            mr={20}
+            display={{ base: "none", lg: "block" }}
+            maxW={"300px"}
+            border={"1px solid red"}
+          >
+            Suggested Users
+          </Box>
+        </Flex>
+      </Container>
+    </>
+  );
+};
+
+export default HomePage;
